7e82975c0e5e4156568228d0e63a02098ab888f8
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-09-25 14:18:09 +0200

    added dbset to context

---
+++ FullStackExercise.Data.Access/AdventureWorksContext.cs
@@ -0,0 +1,17 @@
+﻿using FullStackExercise.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackExercise.Data.Access
+{
+    public class AdventureWorksContext : DbContext
+    {
+        public AdventureWorksContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<SalesOrderHeader> SalesOrderHeaders { get; set; }
+    }
+}
