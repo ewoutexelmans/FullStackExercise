9c8b027536cdfd13f123d19da6d6f9c67657f273
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-09-27 15:48:47 +0200

    provided route to dashboard

--- FullStackExercise.Client/src/app/app-routing.module.ts
+++ FullStackExercise.Client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./customers/customers.module').then(m => m.CustomersModule)
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
