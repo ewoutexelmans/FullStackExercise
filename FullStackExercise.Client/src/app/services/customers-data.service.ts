83030bf4e60343e5bf4d4e4edded5eef614d2644
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-02 12:23:49 +0200

    call to server when we need to get the customers or update the page size

--- FullStackExercise.Client/src/app/services/customers-data.service.ts
+++ FullStackExercise.Client/src/app/services/customers-data.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { CustomersService } from '../api/services';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { filter, flatMap, publish, refCount } from 'rxjs/operators';
+import { GetCustomersByPageResponse } from '../api/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersDataService {
+  private pageSubject$ = new BehaviorSubject<number>(0);
+  private pageSizeSubject$ = new BehaviorSubject<number>(10);
 
-  constructor() { }
+  public response$: Observable<GetCustomersByPageResponse> = combineLatest(
+    this.pageSubject$,
+    this.pageSizeSubject$
+  ).pipe(
+    filter(([page, pageSize]) => page > 0 && pageSize > 0),
+    flatMap(([page, pageSize]) =>
+      this.data.getPagedCustomers$Plain({ page, pageSize })
+    ),
+    publish(),
+    refCount()
+  );
+
+  constructor(private data: CustomersService) {}
+
+  getCustomers(page = 1) {
+    this.pageSubject$.next(page);
+  }
+
+  updatePageCount(pageSize: number) {
+    this.pageSizeSubject$.next(pageSize);
+  }
 }
