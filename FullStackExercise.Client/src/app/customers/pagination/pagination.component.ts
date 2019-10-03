6c46cf6581bdeabc3ebc1d8c85883cf3dc41d8d8
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-03 16:58:20 +0200

    styling

--- FullStackExercise.Client/src/app/customers/pagination/pagination.component.ts
+++ FullStackExercise.Client/src/app/customers/pagination/pagination.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
-  templateUrl: './pagination.component.html'
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
   @Input() pageCount: number;
