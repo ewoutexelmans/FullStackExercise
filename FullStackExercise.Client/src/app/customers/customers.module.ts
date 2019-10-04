1fe23edead82dc435cc15cf938cda36ed1148d37
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-04 13:54:01 +0200

    created filter component

--- FullStackExercise.Client/src/app/customers/customers.module.ts
+++ FullStackExercise.Client/src/app/customers/customers.module.ts
@@ -5,9 +5,10 @@ import { CustomersRoutingModule } from './customers-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { GridComponent } from './grid/grid.component';
 import { PaginationComponent } from './pagination/pagination.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  declarations: [DashboardComponent, GridComponent, PaginationComponent],
+  declarations: [DashboardComponent, GridComponent, PaginationComponent, FilterComponent],
   imports: [CommonModule, CustomersRoutingModule, SharedModule]
 })
 export class CustomersModule {}
