dfe3c521c9795a6de8494c0f3a30d5ba333e2e8c
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-04 15:56:52 +0200

    completed filtercomponent

--- FullStackExercise.Client/src/app/customers/filter/filter.component.ts
+++ FullStackExercise.Client/src/app/customers/filter/filter.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { FilterType } from 'src/app/api/models';
 
 @Component({
   selector: 'app-filter',
-  templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  templateUrl: './filter.component.html'
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent {
+  @Output() changeKeyWord = new Observable<string>();
+  @Output() changeActiveFilters = new EventEmitter<Array<FilterType>>();
+  keyWordSubject$ = new Subject<string>();
+  filters: Array<FilterType> = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.changeKeyWord = this.keyWordSubject$.pipe(
+      debounceTime(1000),
+      distinctUntilChanged()
+    );
   }
 
+  filter(keyWord: string) {
+    this.keyWordSubject$.next(keyWord.trim());
+  }
+
+  toggleFilterType(filterType: FilterType) {
+    if (this.filters.includes(filterType)) {
+      this.filters = this.filters.filter(f => f !== filterType);
+    } else {
+      this.filters.push(filterType);
+    }
+    this.changeFilters();
+  }
+
+  clear() {
+    this.filters = [];
+    this.changeFilters();
+  }
+
+  changeFilters() {
+    this.changeActiveFilters.emit(this.filters);
+  }
 }
