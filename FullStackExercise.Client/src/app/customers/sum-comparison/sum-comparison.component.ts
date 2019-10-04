1649cc37f35bd0ab13bf3ee039f9dae286d285df
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-04 18:25:51 +0200

    sum component implemented

--- FullStackExercise.Client/src/app/customers/sum-comparison/sum-comparison.component.ts
+++ FullStackExercise.Client/src/app/customers/sum-comparison/sum-comparison.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sum-comparison',
-  templateUrl: './sum-comparison.component.html',
-  styleUrls: ['./sum-comparison.component.scss']
+  templateUrl: './sum-comparison.component.html'
 })
-export class SumComparisonComponent implements OnInit {
+export class SumComparisonComponent {
+  @Output() changeHigherLower = new EventEmitter<boolean | null>();
+  @Output() changeSumComparison = new Observable<number>();
 
-  constructor() { }
+  sumComparisonSubject$ = new Subject<number>();
+  higherLower?: boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.changeSumComparison = this.sumComparisonSubject$.pipe(
+      debounceTime(1000),
+      distinctUntilChanged()
+    );
   }
 
+  filter(higherLower?: boolean) {
+    this.higherLower = higherLower;
+    this.changeHigherLower.emit(this.higherLower);
+  }
+
+  updateSum(sum: string) {
+    this.sumComparisonSubject$.next(+sum);
+  }
 }
