9e87d2e75a30cf2c6e32046eb38909309133aaf9
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-04 14:51:05 +0200

    update data service to be able to filter customers

--- FullStackExercise.Client/src/app/services/customers-data.service.ts
+++ FullStackExercise.Client/src/app/services/customers-data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CustomersService } from '../api/services';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { filter, flatMap, publish, refCount } from 'rxjs/operators';
-import { GetCustomersByPageResponse } from '../api/models';
+import { flatMap, publish, refCount } from 'rxjs/operators';
+import { GetCustomersByPageResponse, FilterType } from '../api/models';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,24 @@ import { GetCustomersByPageResponse } from '../api/models';
 export class CustomersDataService {
   private pageIndexSubject$ = new BehaviorSubject<number>(0);
   private pageSizeSubject$ = new BehaviorSubject<number>(10);
+  private keyWordSubject$ = new BehaviorSubject<string>('');
+  private filtersSubject$ = new BehaviorSubject<Array<FilterType>>([]);
 
   public customersPaged$: Observable<
     GetCustomersByPageResponse
-  > = combineLatest(this.pageIndexSubject$, this.pageSizeSubject$).pipe(
-    filter(([pageIndex, pageSize]) => pageIndex >= 0 && pageSize > 0),
-    flatMap(([pageIndex, pageSize]) =>
-      this.data.getPagedCustomers$Json({ pageIndex, pageSize })
+  > = combineLatest(
+    this.pageIndexSubject$,
+    this.pageSizeSubject$,
+    this.keyWordSubject$,
+    this.filtersSubject$
+  ).pipe(
+    flatMap(([pageIndex, pageSize, keyWord, filters]) =>
+      this.data.getPagedCustomers$Json({
+        pageIndex,
+        pageSize,
+        keyWord,
+        filters
+      })
     ),
     publish(),
     refCount()
@@ -30,5 +41,13 @@ export class CustomersDataService {
 
   updatePageSize(pageSize: number) {
     this.pageSizeSubject$.next(pageSize);
+  }
+
+  filterCustomers(keyWord: string) {
+    this.keyWordSubject$.next(keyWord);
+  }
+
+  updateFilters(filters: Array<FilterType>) {
+    this.filtersSubject$.next(filters);
   }
 }
