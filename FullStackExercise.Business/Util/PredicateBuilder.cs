5f8c90bf194fa1f8fdc28d4adbb78b1ece971d38
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-04 13:50:10 +0200

    created predicate builder

---
+++ FullStackExercise.Business/Util/PredicateBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FullStackExercise.Business.Util
+{
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<T, bool>> New<T>() => param => false;
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first,
+            Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.OrElse);
+        }
+
+        private static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second,
+            Func<Expression, Expression, Expression> merge)
+        {
+            var map = first.Parameters
+                .Select((f, i) => new { f, s = second.Parameters[i] })
+                .ToDictionary(p => p.s, p => p.f);
+
+            var secondBody = ParameterReBinder.ReplaceParameters(map, second.Body);
+
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+        private class ParameterReBinder : ExpressionVisitor
+        {
+            private readonly Dictionary<ParameterExpression, ParameterExpression> _map;
+
+            private ParameterReBinder(Dictionary<ParameterExpression, ParameterExpression> map)
+            {
+                _map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+            }
+
+            public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map,
+                Expression exp)
+            {
+                return new ParameterReBinder(map).Visit(exp);
+            }
+
+            protected override Expression VisitParameter(ParameterExpression p)
+            {
+                if (_map.TryGetValue(p, out var replacement))
+                {
+                    p = replacement;
+                }
+
+                return base.VisitParameter(p);
+            }
+        }
+    }
+}
