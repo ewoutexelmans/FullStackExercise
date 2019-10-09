ec05d2ae9bbb254912a82487c8cdbc29b38d6e16
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-09 14:26:42 +0200

    editable field in grid

--- FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
+++ FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-editable-field',
   templateUrl: './editable-field.component.html',
   styleUrls: ['./editable-field.component.scss']
 })
-export class EditableFieldComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class EditableFieldComponent {
+  @Input() value: string;
 }
