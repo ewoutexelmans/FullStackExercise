ddd1cb99418a8a2ddc9c9e2a7fd4e175e42a877e
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-09 16:06:46 +0200

    changed editable field implementation

--- FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
+++ FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-editable-field',
-  templateUrl: './editable-field.component.html',
-  styleUrls: ['./editable-field.component.scss']
+  templateUrl: './editable-field.component.html'
 })
 export class EditableFieldComponent {
   @Input() value: string;
+  @Output() valueChange = new EventEmitter<string>();
+  @Input() editing: boolean;
+
+  onKey(value: string) {
+    this.value = value;
+  }
 }
