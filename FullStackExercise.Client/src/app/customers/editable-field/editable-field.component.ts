f909cab49fba98b4a68ee087270665098d3e1c85
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-10-10 10:38:38 +0200

    added form to grid

--- FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
+++ FullStackExercise.Client/src/app/customers/editable-field/editable-field.component.ts
@@ -1,15 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-editable-field',
   templateUrl: './editable-field.component.html'
 })
 export class EditableFieldComponent {
-  @Input() value: string;
-  @Output() valueChange = new EventEmitter<string>();
+  @Input() parentForm: FormGroup;
+  @Input() placeholder: { name: string; value: string };
   @Input() editing: boolean;
-
-  onKey(value: string) {
-    this.value = value;
-  }
 }
--- FullStackExercise.Client/src/app/customers/grid/grid.component.ts
+++ FullStackExercise.Client/src/app/customers/grid/grid.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CustomerLookupDto } from 'src/app/api/models';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent {
+export class GridComponent implements OnInit {
   @Input() customers: Array<CustomerLookupDto>;
+
   editing = 0;
+  form = this.formBuilder.group({
+    firstName: [''],
+    lastName: ['']
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form.valueChanges.subscribe(newVal => console.log(newVal));
+  }
 
   toggleEditing(id: number) {
     this.editing = id;
